83b3d3d87bce61fe1c8d44431e94b97d5a5f3264
Eric <eric.vasquez3327@gmail.com>
2019-02-25 16:22:43 -0600

    finished PostController

--- src/main/java/com/codeup/blog/MathController.java
+++ src/main/java/com/codeup/blog/MathController.java
@@ -14,19 +14,22 @@ public class MathController {
     @GetMapping("/subtract/{number1}/from/{number2}")
     @ResponseBody
     public int subtract(@PathVariable int number1, @PathVariable int number2){
+
         return number1 - number2;
     }
 
     @GetMapping("/multiply/{number1}/and/{number2}")
     @ResponseBody
-    public int multiply(@PathVariable int number1, @PathVariable int number2){
-        return number1 * number2;
+    public String multiply(@PathVariable int number1, @PathVariable int number2){
+        int product = number1 * number2;
+        return String.valueOf(product);
     }
 
     @GetMapping("/divide/{number1}/by/{number2}")
     @ResponseBody
-    public int divide(@PathVariable int number1, @PathVariable int number2){
-        return number1 / number2;
+    public double divide(@PathVariable double number1, @PathVariable double number2){
+      return number1/number2;
+
     }
 }
 
--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,4 +1,38 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class PostController {
+
+
+    @GetMapping("/posts")
+    @ResponseBody
+public String index(){
+        return "posts index page";
+    }
+
+//view an individual post
+    @GetMapping("/posts/{id}")
+            @ResponseBody
+    public String show(@PathVariable long id){
+        return "posts index page" + id;
+    }
+
+    //view the form for creating a post
+    @GetMapping("/posts/create")
+   @ResponseBody
+        public String showForm(){
+        return "view the form for creating a post";
+
+    }
+//create a new post
+    //same URL that shows form is the same as creates
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String create(@RequestParam(name = "title") String title){
+        return "create post with title" + title;
+    }
+
 }
