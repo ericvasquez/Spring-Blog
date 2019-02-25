42c67557b67f339f54bb5469b4cf3bddd325aa03
Eric <eric.vasquez3327@gmail.com>
2019-02-25 13:42:58 -0600

    changed RequestMapping to Get

--- src/main/java/com/codeup/blog/MathController.java
+++ src/main/java/com/codeup/blog/MathController.java
@@ -1,4 +1,32 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class MathController {
+    @GetMapping( "/add/{number1}/and/{number2}")
+    @ResponseBody
+    public int add(@PathVariable int number1, @PathVariable int number2){
+        return number1 + number2;
+    }
+
+    @GetMapping("/subtract/{number1}/from/{number2}")
+    @ResponseBody
+    public int subtract(@PathVariable int number1, @PathVariable int number2){
+        return number1 - number2;
+    }
+
+    @GetMapping("/multiply/{number1}/and/{number2}")
+    @ResponseBody
+    public int multiply(@PathVariable int number1, @PathVariable int number2){
+        return number1 * number2;
+    }
+
+    @GetMapping("/divide/{number1}/by/{number2}")
+    @ResponseBody
+    public int divide(@PathVariable int number1, @PathVariable int number2){
+        return number1 / number2;
+    }
 }
+
