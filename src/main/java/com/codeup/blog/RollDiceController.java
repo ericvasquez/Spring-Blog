cbcd5ae814479a074277c8178ad7eebaf08c5ee4
Eric <eric.vasquez3327@gmail.com>
2019-02-26 14:11:17 -0600

    first part of roll-dice

---
+++ src/main/java/com/codeup/blog/RollDiceController.java
@@ -0,0 +1,28 @@
+package com.codeup.blog;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+       public String roll(){
+        return "roll-dice";
+    }
+
+    @GetMapping("roll-dice/{n}")
+    public String guessCheck(@PathVariable int n, Model model){
+        int random = (int) Math.ceil(Math.random () *6 );
+        model.addAttribute("random", random);
+        model.addAttribute("n", n);
+      boolean guess = false;
+        if(random == n){
+        guess=true;
+        }
+        model.addAttribute("guess", guess);
+        return "results";
+    }
+}
