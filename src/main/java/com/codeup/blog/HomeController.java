ac2c0ef935bde2946a0d37903ecbae78e6d92ba1
Eric <eric.vasquez3327@gmail.com>
2019-02-26 16:02:53 -0600

    first commit

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
 @Controller
 public class HomeController {
 
@@ -12,4 +14,9 @@ public class HomeController {
     public String home(){
         return "This is the landing page!";
     }
-}
+
+
+
+
+    }
+
