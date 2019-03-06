be03976d7678146ee30b24d980652937199c2101
Eric <eric.vasquez3327@gmail.com>
2019-03-06 15:07:14 -0600

    first commit for Authentication

--- src/main/java/com/codeup/blog/models/User.java
+++ src/main/java/com/codeup/blog/models/User.java
@@ -24,6 +24,16 @@ public class User {
     }
 
 
+
+        public User(User copy) {
+            id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+            email = copy.email;
+            username = copy.username;
+            password = copy.password;
+        }
+
+
+
     public User(String username, String password, String email){
         this.username = username;
         this.password = password;
